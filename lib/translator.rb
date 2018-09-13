c6e9afc156ac286b1852ff4bfec1145747023f7b
Cristina Murillo <cmurill1@jhu.edu>
2018-09-13 19:18:50 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library(file_path)
     emote_hash["get_meaning"][symbols_array[1]]=meaning
     emote_hash["get_emoticon"][symbols_array[0]]=symbols_array[1]
   }
+  emote_hash 
 end
 
 def get_japanese_emoticon
