8563370a701cb548931c037bc5a22cb388e0ee74
Cristina Murillo <cmurill1@jhu.edu>
2018-09-13 19:23:20 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,9 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)["get_emoticon"].each { |japanese_emote, english_emote|
+    
+    
+  }
 end
 
 def get_english_meaning
