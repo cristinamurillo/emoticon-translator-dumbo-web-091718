465ab739b57c0ab664429803eda2ea699e366b77
Cristina Murillo <cmurill1@jhu.edu>
2018-09-13 19:29:01 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 
 def get_english_meaning(file_path, emoticon)
     load_library(file_path)["get_meaning"].each { |english_emote, meaning|
-      if english_emote == emoticon 
+      if english_emote == meaning  
         return 
       end
   }
